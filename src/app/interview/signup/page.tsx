4dc9586a719537043f3830cd527886239e2fe842
Muhammadsayyam317 <sayyambutt317@gmail.com>
2025-03-26 21:01:05 +0500

    delete card

--- src/app/interview/signup/page.tsx
+++ src/app/interview/signup/page.tsx
@@ -7,19 +7,21 @@ export default function Signup() {
         <InterviewLayout
             subtitle="Sign Up to Share Your AI-Generated Interview"
             description="Get started with a 60-day free trial - no credit required!"
+            showStepper={false}
+            showGoogleLogin={true}
+            useCard={false}
         >
-            <div className="w-full">
+
+            <div className="w-full items-center">
                 <SignupForm />
                 <div className="flex justify-center mt-8">
-                {/* <Button className="w-80 bg-transparent text-black hover:bg-transparent border-2 border-solid border-gray-500 flex items-center justify-center gap-2">
-                <Image src="/images/google.png" alt="Google Icon" width={20} height={20} />
-                {buttonText}
-            </Button> */}
+
                     <Button className="w-60"  type="submit">
-                        Sign Up with Google
+                        Sign Up to Continue
                     </Button>
     </div>
             </div>
         </InterviewLayout>
     )
 }
+
