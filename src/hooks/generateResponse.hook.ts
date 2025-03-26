3c86b3fb470f8c6d81d985b1a6e7382ec5dc07a5
Muhammadsayyam317 <sayyambutt317@gmail.com>
2025-03-26 20:57:41 +0500

    add generate reponse api

--- src/hooks/generateResponse.hook.ts
+++ src/hooks/generateResponse.hook.ts
@@ -1,29 +1,29 @@
 import { useMutation } from "@tanstack/react-query";
 import { Generate } from "@/Routes/api.routes";
-import { useRouter } from 'next/navigation'
+import { useRouter } from 'next/navigation';
 import useStore from "@/store/home.store";
-import { toast } from "sonner"
+import { toast } from "sonner";
 
-export default function GenerateResponse() {
+export default function useGenerateResponse() {
     const router = useRouter();
-    const { setJobId } = useStore(); 
+    const { setJobId } = useStore();
 
     return useMutation({
         mutationFn: Generate,
         onSuccess: async (data) => {
             if (data?.id) {
-                setJobId(data.id); 
-                router.push(`/interview?job_id=${data.id}`); 
+                setJobId(data.id);
+                router.push(`/interview?job_id=${data.id}`);
             } else {
-                toast( "Error",{
+                toast("Error", {
                     description: "Invalid job data received",
                 });
             }
         },
         onError: () => {
-            toast( "Error",{
-                description: "Failed to Generate Response",
+            toast("Error", {
+                description: "Failed to generate response",
             });
-        }
+        },
     });
 }
