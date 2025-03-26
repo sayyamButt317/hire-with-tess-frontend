5e4574f4125c671920b41b6f1f594b2ac50d6784
Muhammadsayyam317 <sayyambutt317@gmail.com>
2025-03-26 20:56:37 +0500

    create function to get job detail

--- src/hooks/FetchJobDetails.hook.ts
+++ src/hooks/FetchJobDetails.hook.ts
@@ -1,10 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { GetJobDetails } from "@/Routes/api.routes";
 
-export default function FetchJobDetails(jobId?: string) {
+export default function useFetchJobDetails(jobId?: string) {
     return useQuery({
         queryKey: ["jobDetails", jobId],
-        queryFn: () => GetJobDetails(jobId!),
-        enabled: !!jobId, 
+        queryFn: () => jobId ? GetJobDetails(jobId) : Promise.reject("No job ID provided"),
+        enabled: !!jobId,
+        retry: 2,
     });
 }
