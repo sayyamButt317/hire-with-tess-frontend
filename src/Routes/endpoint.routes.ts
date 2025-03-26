75b188adb277cea71209eb71784e3aa84ba6cb5a
Muhammadsayyam317 <sayyambutt317@gmail.com>
2025-03-26 20:55:46 +0500

    add backend routes

--- src/Routes/endpoint.routes.ts
+++ src/Routes/endpoint.routes.ts
@@ -1,8 +1,24 @@
 
 const baseUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
-
+console.log(baseUrl)
 export const APIEndpoint={
+
+    // jOB Management
+
     GENERATE_JOB_DETAILS:`${baseUrl}/api/v1/generate-job-details/`,
     GENERATE_QUESTION:`${baseUrl}/api/v1/generate-questions/{job_id}`,
-    GET_JOB_DETAILS:`${baseUrl}/api/v1/generate-job-details/{job_id}`
+    GET_JOB_DETAILS:`${baseUrl}/api/v1/job/{job_id}`,
+    GET_INTERVIEW_LINK:`${baseUrl}/api/v1/generate-interview-link/{job_id}`,
+
+    // Interview Management
+
+    INTERVIEW:`${baseUrl}/api/v1/interview/{job_id}`,
+    INTERVIEW_SUBMIT:`${baseUrl}/api/v1/submit-interview/`,
+    INTERVIEW_ANALYZER:`${baseUrl}/api/v1/analyze/{interview_id}`,
+
+    // Authentication
+    SIGNUP:`${baseUrl}/api/v1/auth/signup`,
+    LOGIN:`${baseUrl}/api/v1/auth/login`,
+    GOOGLE_LOGIN:`${baseUrl}/api/v1/auth/google-login`,
+
 }
