2b9d22c8b2c70d973eba54a711aa0af96a9402df
Muhammadsayyam317 <sayyambutt317@gmail.com>
2025-03-27 15:45:00 +0500

    update design

--- src/app/interview/component/placeholder.tsx
+++ src/app/interview/component/placeholder.tsx
@@ -13,13 +13,13 @@ export default function Placeholder({
         <div className="flex justify-center w-full px-4">
             <form onSubmit={onSubmit} className="relative w-full max-w-xl">
                 <Input
-                    className="w-full h-14 bg-white rounded-full placeholder:text-gray-500 pl-4 sm:pl-6 pr-16 sm:pr-20 text-sm sm:text-base"
+                    className="w-[640px] h-[72px] font-normal bg-white rounded-full placeholder:text-gray-500 pl-4 sm:pl-6 pr-16 sm:pr-20 text-sm sm:text-base"
                     placeholder="Describe what you need..."
                     onChange={onChange}
                 />
                 <Button
                     type="submit"
-                    className="absolute cursor-pointer right-2 top-1/2 transform -translate-y-1/2 h-12 bg-orange-400 text-white px-4 sm:px-5 py-2 rounded-full text-sm sm:text-base"
+                    className="absolute font-semibold cursor-pointer -right-14 top-1/2 transform -translate-y-1/2 h-[60px] w-[140px] bg-orange-400 text-white px-4 sm:px-5 py-2 rounded-full text-sm sm:text-base"
                 >
                     Generate
                 </Button>
