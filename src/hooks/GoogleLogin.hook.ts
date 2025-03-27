11329d16778a631633d67df4264f1b251fab38fb
Muhammadsayyam317 <sayyambutt317@gmail.com>
2025-03-27 19:57:39 +0500

    post google login

---
+++ src/hooks/GoogleLogin.hook.ts
@@ -0,0 +1,20 @@
+import {useMutation} from "@tanstack/react-query";
+import {toast} from "sonner";
+import {GoogleLoginIn} from "@/Routes/api.routes";
+
+export default function useGoogleLoginHook() {
+    return useMutation({
+        mutationFn: GoogleLoginIn,
+        onSuccess: async () => {
+            toast.success("Login Successful", {
+                position: "bottom-right",
+            });
+        },
+        onError: () => {
+            toast.error("Login Failed", {
+                description: "Something went wrong",
+                position: "bottom-right",
+            });
+        },
+    });
+}
