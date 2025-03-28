3a09abf619db3a7419ee62e3976f8fc90abbfc32
Muhammadsayyam317 <sayyambutt317@gmail.com>
2025-03-28 14:27:16 +0500

    remove bg

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -16,8 +16,8 @@ export default function Signup() {
             showStepper={false}
             showGoogleLogin={false}
             useCard={false}
-            subtitleClassName="font-[Roboto] font-medium text-[30px] mt-6"
-            descriptionClassName="font-[Roboto] font-medium text-[24px] text-[#606778] mt-4"
+            subtitleClassName="font-[roboto] font-medium text-[30px] mt-6 w-[607px]"
+            descriptionClassName="font-[roboto] font-medium text-[24px] leading-[24px] text-[#606778] mt-4 w-[642px]"
         >
             <div className="flex justify-center">
                 <Button
