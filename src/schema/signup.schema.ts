15057925ea7f5c7c1ebca3d60e3005adaf6c7d7c
Muhammadsayyam317 <sayyambutt317@gmail.com>
2025-03-28 22:07:33 +0500

    update the schema messages

--- src/schema/signup.schema.ts
+++ src/schema/signup.schema.ts
@@ -5,7 +5,7 @@ export const signupFormSchema = z.object({
     lastname: z.string().min(1, { message: "Last name is required." }),
     organization: z.string().min(1, { message: "Please enter your organization name." }),
     email: z.string().email({ message: "Please enter a valid email address." }),
-    password: z.string().min(6, { message: "Password must be at least 6 characters long." }),
+    password: z.string().min(6, { message: "Password must be at least 8 characters long." }),
     confirmpassword: z.string().min(6, { message: "Confirm password must match the password." })
 }).refine((data) => data.password === data.confirmpassword, {
     message: "Passwords do not match.",
