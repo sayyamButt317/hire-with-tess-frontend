07fd3516d53c53efe620a20b2d587db52cbdde13
sayyamButt317 <sayyambutt317@gmail.com>
2025-04-04 21:43:28 +0500

    add edit in store

--- src/store/InputStore.ts
+++ src/store/InputStore.ts
@@ -1,21 +1,44 @@
 import { create } from "zustand";
 import { devtools, persist } from "zustand/middleware";
 
-interface Skills {
+interface SkillState {
     skills: string[];
+    isEditable: boolean;
+    questions: string[];
+    editableQuestionIndex: number | null;
+
+    setIsEditable: (value: boolean) => void;
     setSkills: (value: string[]) => void;
     removeSkills: (index: number) => void;
+
+    setQuestions: (value: string[]) => void;
+    setEditableQuestionIndex: (index: number | null) => void;
+    updateQuestion: (index: number, value: string) => void;
 }
 
-export const useSkillStore = create<Skills>()(
+export const useSkillStore = create<SkillState>()(
     devtools(
         persist(
             (set) => ({
                 skills: [],
-                setSkills: (value) => set({ skills: [...value] }),
+                isEditable: false,
+                questions: [],
+                editableQuestionIndex: null,
+
+                setIsEditable: (value) => set({ isEditable: value }),
+                setSkills: (value) => set({ skills: value }),
                 removeSkills: (indexToRemove) =>
                     set((state) => ({
-                        skills: [...state.skills.filter((_, index) => index !== indexToRemove)],
+                        skills: state.skills.filter((_, index) => index !== indexToRemove),
+                    })),
+
+                setQuestions: (value) => set({ questions: value }),
+                setEditableQuestionIndex: (index) => set({ editableQuestionIndex: index }),
+                updateQuestion: (index, value) =>
+                    set((state) => ({
+                        questions: state.questions.map((q, i) =>
+                            i === index ? value : q
+                        ),
                     })),
             }),
             { name: "input-storage" }
