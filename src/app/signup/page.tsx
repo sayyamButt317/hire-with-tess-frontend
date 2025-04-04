04966aeac771bab61302c2027f9bb4a1a188fdc1
sayyamButt317 <sayyambutt317@gmail.com>
2025-04-04 21:52:53 +0500

    change the sub heading font weight

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -4,19 +4,25 @@ import SignupForm from "../interview/component/signupform";
 import Image from "next/image";
 import React from "react";
 import useGoogleLoginHook from "@/hooks/GoogleLogin.hook";
+import useAuthStore from "@/store/authToken.store";
 
 export default function Signup() {
     const GoogleLoginMutation = useGoogleLoginHook();
-    return (
 
-        <>
+    const handleGoogleLogin = (googleResponse: any) => {
+        const accessToken = googleResponse?.accessToken; 
+        if (accessToken) {
+            GoogleLoginMutation.mutate(accessToken);  
+        }
+    };
+
+    return (
+        <div>
             <div className="flex flex-col items-center justify-center">
                 <h1 className="font-[roboto] font-medium text-[30px] mt-6 w-[607px]">Sign Up to Share Your AI-Generated Interview</h1>
-                <p className="font-[roboto] font-medium text-[24px] leading-[24px] text-[#606778] mt-4 w-[642px]">Get started with a 60-day free trial - no credit required!</p>
+                <p className="font-[roboto] font-[400] text-[24px] leading-[24px] text-[#606778] mt-4 w-[642px]">Get started with a 60-day free trial - no credit required!</p>
                 <Button
-                    onClick={() => GoogleLoginMutation.mutate(
-                        {accessToken:""}
-                    )}
+                    // onClick={() => handleGoogleLogin()}
                     className="w-[528px] h-[64px] border-r-[14px] rounded-[14px] border-[1px] mt-10 mb-4 font-[roboto] font-normal bg-transparent text-black
                                hover:bg-transparent border-gray-400 flex items-center justify-center gap-2"
                 >
@@ -25,18 +31,16 @@ export default function Signup() {
                 </Button>
             </div>
 
-
-                <div className="w-full mt-4 mb-8">
-                    <div className="relative flex items-center justify-center w-full">
-                        <hr className="w-full h-[1px] my-8 mb-4 bg-[#CBCAD7] border-0 rounded-sm dark:bg-gray-700" />
-                        <div className="absolute px-4 font-[Open Sans] font-normal text-[18px] space-x-[28px] bg-white text-gray-600 left-1/2 transform -translate-x-1/2 dark:bg-gray-900">
-                            Or
-                        </div>
+            <div className="w-full mt-4 mb-8">
+                <div className="relative flex items-center justify-center w-full">
+                    <hr className="w-full h-[1px] my-8 mb-4 bg-[#CBCAD7] border-0 rounded-sm dark:bg-gray-700" />
+                    <div className="absolute px-4 font-[Open Sans] font-normal text-[18px] space-x-[28px] bg-white text-gray-600 left-1/2 transform -translate-x-1/2 dark:bg-gray-900">
+                        Or
                     </div>
-
-                    <SignupForm/>
                 </div>
 
-        </>
+                <SignupForm />
+            </div>
+        </div>
     );
 }
