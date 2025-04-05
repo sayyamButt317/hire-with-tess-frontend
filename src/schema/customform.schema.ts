9f1e205acb9c55ae0ae868a3d7ad129055d7cc21
Muhammadsayyam317 <sayyambutt317@gmail.com>
2025-04-06 03:25:53 +0500

    update data input limits

--- src/schema/customform.schema.ts
+++ src/schema/customform.schema.ts
@@ -5,7 +5,7 @@ export const customformSchema = z.object({
     jobTitle: z.string().min(1, { message: "Job title is required." }),
     jobType: z.string().min(1, { message: "Please select the job type ." }),
     companyName: z.string().min(2, { message: "Company name must be at least 2 characters long." }),
-    location: z.string().min(3, { message: "Please enter a valid location ." }),
+    location: z.string().min(2, { message: "Please enter a valid location ." }),
     salary: z.string().min(1, { message: "Please enter a valid salary amount." }),
     currency:z.string().min(1, { message: "Please select a currency ." }),
     questions: z.array(z.string()).optional(),
