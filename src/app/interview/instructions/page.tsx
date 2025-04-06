e08217f9ac146acc838fe379b5030e80fc765327
Muhammadsayyam317 <sayyambutt317@gmail.com>
2025-04-06 18:37:44 +0500

    Complete the Interview flow

--- src/app/interview/instructions/page.tsx
+++ src/app/interview/instructions/page.tsx
@@ -1,1 +1,13 @@
-export default function
+import InterviewLayout from "@/components/layout/InterviewLayout";
+
+export default function CandidateInstructions() {
+    return(
+        <InterviewLayout
+            useCard={true}
+            title="Your next oppurtunity starts here!"
+            subtitle="Let's get started.Record your response at your own pace and put your best foot! "
+            showTitle={true} >
+            <div ></div>
+        </InterviewLayout>
+    )
+}
