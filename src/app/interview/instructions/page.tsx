35e5f05de6f0f9b9c71e6629e2b98e7d29c01489
Muhammadsayyam317 <sayyambutt317@gmail.com>
2025-04-06 18:43:21 +0500

    add instruction screens

--- src/app/interview/instructions/page.tsx
+++ src/app/interview/instructions/page.tsx
@@ -3,9 +3,10 @@ import InterviewLayout from "@/components/layout/InterviewLayout";
 export default function CandidateInstructions() {
     return(
         <InterviewLayout
-            useCard={true}
-            title="Your next oppurtunity starts here!"
-            subtitle="Let's get started.Record your response at your own pace and put your best foot! "
+            showStepper={false}
+            useCard={false}
+            description="Let's get started.Record your response at your own pace and put your best foot! "
+            subtitle="Your next opportunity starts here! "
             showTitle={true} >
             <div ></div>
         </InterviewLayout>
