1084621b631ea7570d321e7be3b2fa8847de54ef
sayyamButt317 <sayyambutt317@gmail.com>
2025-04-08 17:04:28 +0500

    add candidate flow

--- src/app/interview/details/[jobId]/page.tsx
+++ src/app/interview/details/[jobId]/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+'use client'
 import InterviewLayout from "@/components/layout/InterviewLayout";
 import { Card } from "@/components/ui/card";
 import { FormField, FormItem } from "@/components/ui/form";
@@ -6,54 +6,29 @@ import CustomInputForm from "@/app/interview/component/customformInput";
 import React, { useRef } from "react";
 import { FormProvider, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import {
-  CandidateDetailSchema,
-  CandidateDetailsValidator,
-} from "@/schema/CandidateDetail.schema";
-import AddUserDetailsHook from "@/hooks/PostUserDetailshook";
+import { CandidateDetailSchema, CandidateDetailsValidator } from "@/schema/CandidateDetail.schema";
 import { Button } from "@/components/ui/button";
-import { useSearchParams } from "next/navigation";
-import Link from "next/link";
+import { useRouter } from "next/navigation";  
+import { useParams } from 'next/navigation';  
 
 export default function CandidatesDetails() {
-    const searchParams = useSearchParams();
-    const job_id = searchParams.get("job_id") || "";
-    console.log(job_id)
+  const { jobId } = useParams();
+  const router = useRouter(); 
+
   const form = useForm<CandidateDetailsValidator>({
     resolver: zodResolver(CandidateDetailSchema),
     defaultValues: {
-    candidate_name : "",
+      candidate_name: "",
       email: "",
       phone: "",
       image: "", 
-    //   job_id: job_id,
     },
   });
 
-
-//   const UserDetailmutation = AddUserDetailsHook(); 
-
-  const onSubmit = async () => {
-    // UserDetailmutation.mutate({
-    //     // job_id,
-    //     candidate_name: values.candidate_name  || "",
-    //   email: values.email || "",
-    //   phone: values.phone || "",
-    //   image: values.image || "", 
-    // });
-
+  const onSubmit = async (data: CandidateDetailsValidator) => {
+    console.log(data);
+    router.push(`/interview/candidate-question`);
   };
-
-
-  const convertToBase64 = (file: File): Promise<string> => {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onload = () => resolve(reader.result as string);
-      reader.onerror = (error) => reject(error);
-    });
-  };
-  
 
   const ref = useRef<HTMLFormElement>(null);
 
@@ -64,93 +39,69 @@ export default function CandidatesDetails() {
       subtitle="Fill In Your Details!"
       description="Please fill out your details before starting your Interview"
     >
-        <FormProvider {...form}>
-      <div>
-        <Card className="border-dashed border-[#6F6C90] h-[159px] px-4 items-center justify-center">
-        <FormField
-            control={form.control}
-            name="image"
-            render={({ field }) => (
-              <FormItem>
-                <input
-                  type="file"
-                  accept="image/*"
-                  onChange={async (e) => {
-                    const file = e.target.files?.[0];
-                    if (file) {
-                      const base64 = await convertToBase64(file);
-                      form.setValue("image", base64);
-                    }
-                  }}
-                />
-              </FormItem>
-            )}
-          />
-        </Card>
-      </div>
-      <input type="hidden" value={job_id} {...form.register("job_id")} />
+      <FormProvider {...form}>
+        <div>
+          <Card className="border-dashed border-[#6F6C90] h-[159px] px-4 items-center justify-center">
+            <FormField
+              control={form.control}
+              name="image"
+              render={({ field }) => (
+                <FormItem>
+                  <input
+                    type="file"
+                    accept="image/*"
+                    onChange={(e) => field.onChange(e.target.files?.[0])}
+                  />
+                </FormItem>
+              )}
+            />
+          </Card>
+        </div>
 
-      <div className="mt-8">
-      
-        <form onSubmit={form.handleSubmit(onSubmit)} ref={ref} className="space-y-8 px-4">
-          <FormField
-            control={form.control}
-            name="candidate_name"
-            render={({ field }) => (
-              <CustomInputForm
-                {...field}
-            
-                label="Your Name"
-                placeholder="Name"
-              />
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              
+        <div className="mt-8">
+          <form onSubmit={form.handleSubmit(onSubmit)} ref={ref} className="space-y-8 px-4">
+            <FormField
+              control={form.control}
+              name="candidate_name"
+              render={({ field }) => (
                 <CustomInputForm
                   {...field}
-                  
-                  label="Email"
-                  placeholder="email"
+                  label="Your Name"
+                  placeholder="Name"
                 />
-            
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="phone"
-            render={({ field }) => (
-              
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
                 <CustomInputForm
                   {...field}
-                  name="PhoneNumber"
+                  label="Email"
+                  placeholder="Email"
+                />
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="phone"
+              render={({ field }) => (
+                <CustomInputForm
+                  {...field}
                   label="Phone Number"
                   placeholder="Phone Number"
-                  
                 />
-        
-            )}
-          />
+              )}
+            />
           
-            <Link href={"/interview/candidate-question"}>
-            <Button type="submit"
-   
-   className="w-full sm:w-[381px] h-[64px] leading-[20px] font-roboto cursor-pointer rounded-2xl max-w-[90%]" >
- Start Interview
-   </Button></Link>
-          
-
-          {/* <Button type="submit"
-           disabled={UserDetailmutation.isPending}
-          className="w-full sm:w-[381px] h-[64px] leading-[20px] font-roboto cursor-pointer rounded-2xl max-w-[90%]" >
-           {UserDetailmutation.isPending ? "Interview Starting.." : "Start Interview"} 
-          </Button> */}
-        </form>
-   
-      </div>
+            <Button
+              type="submit"
+              className="w-full sm:w-[381px] h-[64px] leading-[20px] font-roboto cursor-pointer rounded-2xl max-w-[90%]"
+            >
+              Start Interview
+            </Button>
+          </form>
+        </div>
       </FormProvider>
     </InterviewLayout>
   );
--- src/hooks/PostUserDetailshook.ts
+++ src/hooks/PostUserDetailshook.ts
@@ -1,19 +1,16 @@
 import { UserDetails } from "@/Routes/api.routes";
 import { useMutation } from "@tanstack/react-query";
-import { useRouter } from "next/navigation";
-import useHomeStore from "@/store/home.store";
+import { useRouter } from "next/navigation";  // Use the right router
 import { toast } from "sonner";
 import { AxiosError } from "axios";
 
 export default function AddUserDetailsHook() {
   const router = useRouter();
-  const { setJobId } = useHomeStore();
 
   return useMutation({
     mutationFn: UserDetails,
     onSuccess: async (data) => {
       if (data?.id) {
-        setJobId(data.id);
         router.push(`/interview/candidate-question?interview_id=${data.id}`);
       } else {
         toast("Invalid Details");
